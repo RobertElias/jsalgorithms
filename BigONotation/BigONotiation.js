d057982db3b86ee250a01d546e102242a3f47fba
RobertElias <robert.elias@live.com>
2020-01-09 17:56:46 -0800

    BigONotation file created

---
+++ BigONotation/BigONotiation.js
@@ -0,0 +1,20 @@
+//Add up to N
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+//Should render a specific time lapse...
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+console.log(addUpTo(6));
