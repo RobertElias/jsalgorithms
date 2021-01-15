354a700081cba903fcc0912ad8f051031fcea73a
robertelias <robert.elias@live.com>
2021-01-14 17:16:44 -0800

    datastructures

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -21,6 +21,7 @@ function fizzBuzz(n) {
       // Is the number a multiple of 3?
       console.log('fizz');
     } else if (i % 5 === 0) {
+      // Is the number a multiple of 5?
       console.log('buzz');
     } else {
       console.log(i);
